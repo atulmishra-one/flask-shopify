fd5335eb9c8ab4e3d8ed646ffcda248ffcd2471f
atulmishra-one <atulmishra.one@gmail.com>
2016-10-22 20:01:31 +0530

    Create Config classes

--- flask_ecommerce/config/local.py
+++ flask_ecommerce/config/local.py
@@ -1,1 +1,5 @@
-DEBUG = True
+from flask_ecommerce.config.base import Base
+
+
+class LocalConfig(Base):
+    DATABASE_URI = ''
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask_ecommerce.application import initialize_app
-from flask_ecommerce.config import local
+from flask_ecommerce.config.local import LocalConfig
 
 
-flask_ecommerce = initialize_app(local)
+flask_ecommerce = initialize_app(LocalConfig)
 
 flask_ecommerce.run(threaded=True)
